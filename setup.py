125ccddf3fde60be605b08e4a7899771c32cb490
doomedraven <doomedraven@users.noreply.github.com>
2016-01-15 21:07:47 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='vt',
-    version='2.1.0.3',
+    version='2.1.0.4',
     description='VirusTotal Full API',
     license='For fun :)',
     packages=find_packages(),
